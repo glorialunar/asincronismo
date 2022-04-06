75e909656b5b4d7cddd2332de98a3cbe7ae2a547
glorialunar <glorialunar2509@gmail.com>
2022-04-06 14:47:50 -0300

    Entendiendo callbacks y setTimeout

---
+++ src/callback/index.js
@@ -0,0 +1,37 @@
+// Entendiendo callbacks
+function sum (num1, num2) {
+    return num1 + num2;
+}
+
+function calc (num1, num2, callback) {
+    return callback (num1, num2);
+}
+
+console.log(calc(5,550,sum));
+
+// Entendiendo setTimeout
+function date() {
+    console.log("Primer console.log", new Date);
+    setTimeout(function (){
+        console.log("Segundo console.log", new Date);
+    }, 3000)
+    console.log("Tercer console.log", new Date);
+}
+
+date();
+
+// Usando setTimeout con callback
+function date(callback) {
+    console.log("Primer console.log", new Date);
+    setTimeout(function (){
+        let date = new Date;
+        callback(date);
+    }, 3000)
+}
+
+function printDate (dateNow) {
+    console.log("Segundo console.log", dateNow);
+}
+
+date(printDate);
+
